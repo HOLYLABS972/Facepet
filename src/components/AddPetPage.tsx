86dd091ddf17bc0398d5534ed38e50773c86f3a4
Dmitry Polskoy <96145205+AAAxis@users.noreply.github.com>
2025-09-02 18:49:14 +0300

    looki

--- src/components/AddPetPage.tsx
+++ src/components/AddPetPage.tsx
@@ -1,13 +1,9 @@
 import AddNewPetForm from './AddNewPetForm';
-import Navbar from './layout/Navbar';
 
 export default function AddPetPage() {
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
-      <div className="pt-20">
-        <AddNewPetForm />
-      </div>
+      <AddNewPetForm />
     </div>
   );
 }
